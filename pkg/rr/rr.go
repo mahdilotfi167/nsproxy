0645962e36de49dbdc0e91ecddbadec0611d4575
mahdilotfi167 <mahdi.vahshat.80@gmail.com>
2023-06-26 17:47:12 +0330

    Fix RR type definitions

--- pkg/rr/rr.go
+++ pkg/rr/rr.go
@@ -4,17 +4,21 @@ Copyright © 2023 Mahdi Lotfi mahdilotfi167@gmail.com
 
 package rr
 
-const TYPE_A uint16 = 1
-const TYPE_NS uint16 = 2
-const TYPE_CNAME uint16 = 5
-const TYPE_MX uint16 = 15
-const TYPE_TXT uint16 = 16
-const TYPE_AAAA uint16 = 28
-const TYPE_ALL uint16 = 255
+const (
+	TYPE_A     uint16 = 1
+	TYPE_NS    uint16 = 2
+	TYPE_CNAME uint16 = 5
+	TYPE_MX    uint16 = 15
+	TYPE_TXT   uint16 = 16
+	TYPE_AAAA  uint16 = 28
+	TYPE_ALL   uint16 = 255
+)
 
-const CLASS_IN uint16 = 1
-const CLASS_CH uint16 = 3
-const CLASS_ALL uint16 = 255
+const (
+	CLASS_IN  uint16 = 1
+	CLASS_CH  uint16 = 3
+	CLASS_ALL uint16 = 255
+)
 
 type Record struct {
 	Name  string
